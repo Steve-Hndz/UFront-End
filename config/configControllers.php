077d503bec6151f412854a122a5108eba566255b
jCisneros <anju.jjcg@gmail.com>
2020-10-10 17:27:20 -0600

    estilos, header, homepage creados

--- config/configControllers.php
+++ config/configControllers.php
@@ -1,13 +1,13 @@
 <?php
 require_once "database/MySqlConnection.php";
 
-define ('BASE_DIR', "http://127.0.0.1/web/");
+define ('BASE_DIR', "http://127.0.0.1/UFront-End/");
 define ('CONTROLLERS_DIR', "controllers/");
 define ('DEFAULT_CONTROLLER', "Home");
 define ('DEFAULT_ACTION', "showHome");
 
 define ('DB_USER', "root");
-define ('DB_HOST', "127.0.0.1");
+define ('DB_HOST', "localhost");
 define ('DB_PASSWORD', "");
 define ('DB_NAME', "db_bloodproject");
 
--- views/header.php
+++ views/header.php
@@ -1,24 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= BASE_DIR; ?>css/style.css">
+    <!-- <link rel="stylesheet" href="<?= BASE_DIR; ?>assets/css/style.css"> -->
+    <link rel="stylesheet" href="<?= BASE_DIR; ?>assets/css/root.css">
+    <link rel="stylesheet" href="<?= BASE_DIR; ?>assets/css/navbar.css">
+    <link rel="stylesheet" href="<?= BASE_DIR; ?>assets/css/home.css">
     <title>TPI -render con OOP-</title>
 </head>
+
 <body>
     <header>
-        <nav>
-            <h1>
-                <a href="home.php">
-                    PHP ORIENTADO A OBJETOS
+        <nav class="navbar">
+            <div class="navbar__logo">
+                <a href="home.php" class="navbar__a">
+                    INICIO PAGINA
                 </a>
-            </h1>
-        
-            <ul>
-                <li><a href="home.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
+            </div>
+
+            <div class="navbar__navegation">
+                <ul class="navbar__links">
+                    <li class="navbar__link">
+                        <a href="home.php" class="navbar__a navbar__a--selected">Inicio</a>
+                    </li>
+                    <li class="navbar__link">
+                        <a href="about.php" class="navbar__a">Donantes</a>
+                    </li>
+                    <li class="navbar__link">
+                        <a href="contact.php" class="navbar__a">Registrar</a>
+                    </li>
+                    <li class="navbar__link">
+                        <a href="contact.php" class="navbar__a">Acerca de</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
