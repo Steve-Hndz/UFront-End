fe953172d4512f54d692be9e611f12dd1d815244
Alejandro Ibañez <JoseIbaez>
2020-10-18 01:45:51 -0600

    Adaptado para prueba de encriptacion 2.0

--- controllers/PacientesController.php
+++ controllers/PacientesController.php
@@ -15,11 +15,18 @@ class PacientesController
   public function save()
   {
     require_once "models/Donante.php";
+    require_once "models/Paciente.php";
     echo "<main>";
     if (isset($_POST)) {
-      $objeto = new Donante();
+      //EJEMPLO DE ENCRIPACION DE DONANTE
+      $objeto = new Paciente();
+      $objeto->setContraseñaPaciente($_POST['contrasenia']);
+      echo '<h3>Contraseña Encriptada Paciente: ' . $objeto->getContraseñaPaciente() . '</h3>';
+
+      //EJEMPLO DE ENCRIPACION DE DONANTE
+      /* $objeto = new Donante();
       $objeto->setContraseñaDonante($_POST['contrasenia']);
-      echo $objeto->getContraseñaDonante();
+      echo '<h3>Contraseña Encriptada Donante: ' . $objeto->getContraseñaDonante() . '</h3>'; */
     }
     echo "</main>";
   }
