fa7568551d32505657c0d0d5a9a7fdf846cb9d21
Ivan Romero <ivanrmrhdz@gmail.com>
2020-10-24 15:26:56 -0600

    arreglo de estilos
    se arregló el estilo del datatable, la vista donantes es ahora como la vista pacientes, en el responsivo de la vista pacientes el container de la tabla no estaba bien centrado, se corrigió. En la vista pacientes se corrigió el padding de la tabla en el responsivo.

--- views/footer.php
+++ views/footer.php
@@ -1,9 +1,11 @@
     <footer class="footer">
         <p class="footer__text">ADS - All rights reserved © 2020</p>
     </footer>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.js"></script>
-    <script type="text/javascript" src="<?=BASE_DIR;?>assets/js/main.js"></script>
+    
+
+    
 </body>
 
 </html>
