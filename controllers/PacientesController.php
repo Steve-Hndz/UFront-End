1176b6b2175864004f318540c0534f6d61777721
Erick Saravia <erickjosue8916@gmail.com>
2020-10-25 21:01:44 -0600

    fix: (pacientes) mostrando estado paciente en la tabla

--- controllers/PacientesController.php
+++ controllers/PacientesController.php
@@ -34,7 +34,7 @@ class PacientesController
     }
     $donantes = new Paciente();
     $list = $donantes->list($page, $limit, $filter, $sort);
-    // var_dump($list);
+    var_dump($list);
     
     require_once 'views/pacientes.php';
   }
