745fe6d41562b3a3165b0af647ef0a9f03bee9d5
Javier Morales <mm18014@ues.edu.sv>
2020-10-30 08:01:49 -0600

    fixeado el icono de la pagina

--- views/header.php
+++ views/header.php
@@ -23,7 +23,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="assets/images/jeringa.svg">
+    <link rel="shortcut icon" href="<?= BASE_DIR?>assets/images/jeringa.svg">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
     <link rel="stylesheet" href="<?= BASE_DIR; ?>assets/css/dataTable.css">
     <link rel="stylesheet" href="<?= BASE_DIR; ?>assets/css/root.css">
